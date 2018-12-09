7c190e0cafdeab1ed7da474cb99d1a6bec9fd629
samyakjain677 <44441096+samyakjain677@users.noreply.github.com>
2018-12-09 21:29:21 +0530

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "../css/main.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoStore from "./TodoStore";
+import TodoList from "./TodoList";
+
+const app = document.getElementById("root");
+ReactDOM.render(<TodoList store={TodoStore} />, app);
